de04ba6fd9475cd3ee0af24d1ed064c06f2fa116
Emmanuel Oreofe Oyewole <nathanemmanueloreofe@outlook.com>
2025-11-16 23:34:38 +0100

    update: started building the main display page on browser!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React, { JSX } from 'react'
 export default function HomePage(): JSX.Element {
   return (
     <div>
-      <h1>Welcome to beg3, where you build, & beg smartly, and shamelessly.</h1>
+      <h1 className="text-center px-4">Welcome to beg3, where you build, & beg smartly, and shamelessly.</h1>
     </div>
   )
 }
